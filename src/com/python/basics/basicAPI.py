bb5c92029c12075e592229863094fb81ee8975f7
Pratyush <pratyusk@amazon.com>
2018-05-20 13:35:31 +0530

    class and __init__ implementation

--- src/com/python/basics/basicAPI.py
+++ src/com/python/basics/basicAPI.py
@@ -22,7 +22,48 @@ def whatTimeIsIt():
     localtime = time.asctime(time.localtime(time.time()))
     return localtime
 
-#reads input from user and prints on screen
+
+# reads input from user and prints on screen
 def readInputFromUser():
     name = raw_input("Please type your name: ");
     print "Hi ", name
+
+
+# Returns temperature in fahrenheit
+def KelvinToFahrenheit(Temperature):
+    return ((Temperature - 273) * 1.8) + 32
+  
+  
+# 100 will be divided by number and result would be printed 
+def tryExceptDemo(number):
+    try:
+        i = 100 / number
+    except ZeroDivisionError:
+        print "Numbers can't be devide by zero"
+    else:
+        print "Number after division: ", i
+
+        
+# 100 will be divided by number and result would be printed 
+def tryExceptFinllyDemo(number):
+    try:
+        i = 100 / number
+    except ZeroDivisionError:
+        print "Numbers can't be devide by zero"
+    else:
+        print "Number after division: ", i
+    finally:
+        print "finally block will always be executed!!!"
+
+
+
+class basics:
+    
+    def __init__(self, name):
+        print "Name is ", name
+    def test(self):
+        print "test api in basics class"
+        
+base=basics("Pratyush")
+base.test()
+
--- src/com/python/test/apiCall.py
+++ src/com/python/test/apiCall.py
@@ -7,4 +7,18 @@ from com.python.basics import basicAPI
 
 print "Current Time: ", basicAPI.whatTimeIsIt()
 print "Addition of Numbers is: ", basicAPI.add(10, 37)
-basicAPI.readInputFromUser()
+#basicAPI.readInputFromUser()
+print basicAPI.KelvinToFahrenheit(100)
+print basicAPI.KelvinToFahrenheit(-100)
+
+print "\n"
+basicAPI.tryExceptDemo(20)
+basicAPI.tryExceptDemo(0)
+
+print "\n"
+basicAPI.tryExceptFinllyDemo(20)
+basicAPI.tryExceptFinllyDemo(0)
+
+print "\n"
+base=basicAPI.basics("Pratyush")
+base.test()
